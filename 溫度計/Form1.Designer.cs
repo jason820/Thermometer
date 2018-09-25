1a84c7228a6270d00bb12f128b16bad10585268c
Jason <jason18011@qq.com>
2018-09-25 17:53:45 +0800

    美化顯示畫面，啓動後固定到畫面右上角，改變內部處理方式解決內內存泄漏問題

---
+++ 溫度計/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace 溫度計
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "T1:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "T2:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("新細明體", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(99, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 28);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "10.0°C";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("新細明體", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(100, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 28);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "10.0°C";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::溫度計.Properties.Resources.Thermometer_64px_1187299_easyicon_net;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(74, 73);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(212, 80);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "溫度計 v.2.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ 溫度計/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Drawing;
+using System.IO.Ports;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace 溫度計
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            notifyIcon1.Text = this.Text;
+            notifyIcon1.Icon = this.Icon;
+            this.ShowInTaskbar = false;
+
+            this.zabbix_server_ip = System.Configuration.ConfigurationManager.AppSettings["zabbix_server_ip"];
+            if (this.zabbix_server_ip != null)
+            {
+                this.zabbix_host = System.Configuration.ConfigurationManager.AppSettings["zabbix_host"];
+                this.t1key = System.Configuration.ConfigurationManager.AppSettings["t1.key"];
+                this.t2key = System.Configuration.ConfigurationManager.AppSettings["t2.key"];
+                timer1.Interval = 10 * 1000;
+                timer1.Enabled = true;
+            }
+
+            //获取当前工作区宽度和高度（工作区不包含状态栏）
+            int ScreenWidth = Screen.PrimaryScreen.WorkingArea.Width;
+            int ScreenHeight = Screen.PrimaryScreen.WorkingArea.Height;
+            //计算窗体显示的坐标值，可以根据需要微调几个像素
+            int x = ScreenWidth - this.Width - 5;
+            int y = 5; // ScreenHeight - this.Height - 5;
+            this.Location = new Point(x, y);
+        }
+
+        SerialPort serialPort = null;
+
+        string zabbix_server_ip = "";
+        string zabbix_host = "";
+        string t1key = "";
+        string t2key = "";
+
+        string _t1 = "";
+        string _t2 = "";
+
+        public void InitSerialPort()
+        {
+            serialPort = new SerialPort();
+            if (serialPort != null)
+            {
+                serialPort.PortName = System.Configuration.ConfigurationManager.AppSettings["port"];//端口号，这里可以电脑已经连接的COM口，如COM1;
+                serialPort.DataReceived += SerialPort_DataReceived;
+            }
+            if (!serialPort.IsOpen)
+            {
+                serialPort.Open();//打开端口，进行监控
+            }
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            Regex regex = new Regex(@"t([12]{1})=[+-]{1}([0-9.]+)", RegexOptions.IgnoreCase);
+            Match m = regex.Match(indata);
+            if (m.Success)
+            {
+                if (m.Groups[1].Value == "1")
+                {
+                    this._t1 = m.Groups[2].Value;
+                }
+                else
+                {
+                    this._t2 = m.Groups[2].Value;
+                }
+
+                this.Invoke((EventHandler)(delegate
+                {
+                    this.label4.Text = this._t1 + "°C";
+                    this.label3.Text = this._t2 + "°C";
+                }));
+            }
+        }
+
+        [DllImport("shell32.dll ")]
+        public static extern int ShellExecute(IntPtr hwnd, StringBuilder lpszOp, StringBuilder lpszFile, StringBuilder lpszParams, StringBuilder lpszDir, int FsShowCmd);
+
+        private void call_zabbex_send(string t, string temp)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("-vv -z " + this.zabbix_server_ip);
+            sb.Append(" -s " + this.zabbix_host);
+            if (t == "t1")
+                sb.Append(" -k " + this.t1key);
+            else
+                sb.Append(" -k " + this.t2key);
+            sb.Append(" -o " + temp);
+            ShellExecute(IntPtr.Zero, new StringBuilder("Open"), new StringBuilder(System.Configuration.ConfigurationManager.AppSettings["zabbix_sender"]), sb, new StringBuilder(""), 0);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitSerialPort();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.call_zabbex_send("t1", this._t1);
+            this.call_zabbex_send("t2", this._t2);
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+        }
+
+    }
+}
